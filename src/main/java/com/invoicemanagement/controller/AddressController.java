8a09d109882b1d4237bc15098361670e5762b5dd
Deepanshu verma <deepanshu.verma@adjecti.com>
2023-05-03 15:46:12 +0530

    address controller

--- src/main/java/com/invoicemanagement/controller/AddressController.java
+++ src/main/java/com/invoicemanagement/controller/AddressController.java
@@ -1,8 +1,9 @@
 package com.invoicemanagement.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("*")
 @RestController
 public class AddressController {
-
 }
