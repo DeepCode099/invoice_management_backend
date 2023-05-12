14c7b0a9e92c1d8f680bcb4c3840dc9ec1cf4a1d
nikkitri1991 <bitbucket2@adjecti.com>
2023-05-12 12:59:29 +0530

    reflectionutil class

--- src/main/java/com/invoicemanagement/util/ReflectionBeanUtil.java
+++ src/main/java/com/invoicemanagement/util/ReflectionBeanUtil.java
@@ -5,27 +5,28 @@ import java.util.Map;
 
 public class ReflectionBeanUtil {
 	
-	public void myFunction(Map<Object, Object> map, Object entity) throws ClassNotFoundException {
+	public static void mapClassFields(Map<Object, Object> map, Object entity) {
 		for (Field field : entity.getClass().getDeclaredFields()) {
 			field.setAccessible(true);
-			String fieldType = field.getType().getSimpleName();
+				String fieldType =field.getType().getSimpleName();
+				
+				try {
+					if (field.getName() != "id") {
+						if (fieldType.equals("long")) {
 
-			try {
-				if (field.getName() != "id") {
-					if (fieldType.equals("long")) {
+							field.set(entity, Long.parseLong(map.get(field.getName()).toString()));
+						} else if (fieldType.equals("int")) {
 
-						field.set(entity, Long.parseLong(map.get(field.getName()).toString()));
-					} else if (fieldType.equals("int")) {
-
-						field.set(entity, Integer.parseInt(map.get(field.getName()).toString()));
-					} else {
-						field.set(entity, map.get(field.getName()));
+							field.set(entity, Integer.parseInt(map.get(field.getName()).toString()));
+						} else {
+							field.set(entity, map.get(field.getName()));
+						}
 					}
+				} catch (IllegalAccessException e) {
+					e.printStackTrace();
 				}
-			} catch (IllegalAccessException e) {
-				e.printStackTrace();
 			}
-		}
+		
 	}
 
 }
